9465de9b11d73fd651c471e9e502fe0bdc1773bf
Marc McKinley <marc@puppet.com>
2018-10-05 17:56:26 +0100

    Update TestRoutes.java

--- src/test/java/com/puppet/sample/TestRoutes.java
+++ src/test/java/com/puppet/sample/TestRoutes.java
@@ -40,9 +40,9 @@ public class TestRoutes
         assertTrue(res.body.contains("Hello World!"));
 	}
 
-    private TestResponse request(String method, String path, String ip = "10.90.30.185") throws java.io.IOException {
+    private TestResponse request(String method, String path) throws java.io.IOException {
 		try {
-			URL url = new URL("http" + ip + path);
+			URL url = new URL("http://localhost:9999" + path);
 			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 			connection.setRequestMethod(method);
 			connection.setDoOutput(true);
